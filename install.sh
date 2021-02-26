8d785e74cfb632e98698c27d999a74967d22cdad
SekiBetu <realsekibetu@gmail.com>
2021-02-27 00:04:34 +0800

    fix goroot path

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo 'export PATH=$PATH:/usr/local/bin'>>~/.bashrc #修改默认环境变量，�
 export PATH=$PATH:/usr/local/bin
 
 #安装go相关下载工具
-sudo rm -rf /usr/local/go #如果有已经安装的go环境，先卸载，新老版本会有冲突，如不希望可以注释掉
+sudo rm -rf $(go env GOROOT) #如果有已经安装的go环境，先卸载，新老版本会有冲突，如不希望可以注释掉
 wget https://dl.google.com/go/go1.16.linux-amd64.tar.gz #安装go环境，如不希望可以注释掉
 sudo tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz ; rm go1.16.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin'>>~/.bashrc #修改默认环境变量，如不希望可以注释掉
